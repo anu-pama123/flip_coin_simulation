0be951687c903696136b46ef1ba4de589e0f0935
anupama cv <anu.anupamacv@gmail.com>
2021-05-12 22:18:12 +0530

    added flip_coin_simulation.sh

--- flip_coin_simulation.sh
+++ flip_coin_simulation.sh
@@ -1,4 +1,8 @@
 echo "---Flip the coin---"
+#random_number=$(($RANDOM%2))
+
+for ((i=0;i<20;i++))
+do
 random_number=$(($RANDOM%2))
 if [ $random_number -eq 1 ]
 then 
@@ -6,3 +10,4 @@ echo "Head"
 else
 echo "Tail"
 fi
+done
