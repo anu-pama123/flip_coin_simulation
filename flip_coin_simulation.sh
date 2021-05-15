8c169baae9c4dcdcb6ccd78eb0411b8ee5db455f
anupama cv <anu.anupamacv@gmail.com>
2021-05-15 21:05:51 +0530

    added case statement

--- flip_coin_simulation.sh
+++ flip_coin_simulation.sh
@@ -1,5 +1,43 @@
+echo "1)21 times winning program of either head or tail  2)program for tie condition"
+read -p "Enter your choice "choice
 echo "---Flip the coin---"
 
+case $choice in
+1)head_count=0
+tail_count=0
+while [ 1 ]
+do
+random_number=$(($RANDOM%2))
+if [ $random_number -eq 1 ]
+then 
+echo "Head"
+((head_count++))
+else
+echo "Tail"
+((tail_count++))
+fi
+if [ $head_count -eq 21 ] || [ $tail_count -eq 21 ]
+then
+echo "limit reached"
+echo "Head="$head_count"times"
+echo "Tail="$tail_count"times"
+if [ $head_count -eq 21 ]
+then
+result=$(($head_count - $tail_count))
+echo "Head won by $result"
+else
+result1=$(($tail_count - $head_count))
+echo "Tail won by $result1"
+fi
+break
+fi
+done
+;;
+esac
+
+
+case $choice in
+2)
 head_count=0
 tail_count=0
 while [ 1 ]
@@ -21,6 +59,20 @@ echo "Tail="$tail_count"times"
 break
 fi
 done
+;;
+esac
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
